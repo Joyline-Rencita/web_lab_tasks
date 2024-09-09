26d6440265d4718f79e577b4ca39517e7a12a9d9
Joyline Rencita Dsouza <107092284+Joyline-Rencita@users.noreply.github.com>
2024-09-09 17:44:33 +0530

    Create home.php

---
+++ PHP Tasks/home.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>page1
+        </title>
+    </head>
+    <body>
+        <?php
+             echo $_POST["username"].":".$_POST["pswd"],":".$_POST["submit"];
+        ?>
+    </body>
+</html>
